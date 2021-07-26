0b5c35fceaa029710adc224a1cb86613e9a87126
cemaleddin <cemaleddinkuru@gmail.com>
2021-07-26 15:08:01 +0300

    create repo

---
+++ ClientApp/build/precache-manifest.aa577ed7ab89c74e41844278e05003ac.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36050692122a506afa8ced81d4d9cbf6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1c5ecfe2e6a08807c65",
+    "url": "/static/css/2.25b32726.chunk.css"
+  },
+  {
+    "revision": "6cf9378090ec18fc7f33",
+    "url": "/static/css/main.13b298a8.chunk.css"
+  },
+  {
+    "revision": "c1c5ecfe2e6a08807c65",
+    "url": "/static/js/2.9bde12ad.chunk.js"
+  },
+  {
+    "revision": "7e8409f8cb3821d4e1cacd0d340671a3",
+    "url": "/static/js/2.9bde12ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cf9378090ec18fc7f33",
+    "url": "/static/js/main.a4b5c018.chunk.js"
+  },
+  {
+    "revision": "3961b648cc177934aadf",
+    "url": "/static/js/runtime-main.303b0043.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/static/media/notification.c0d3c94c.eot"
+  }
+]);
